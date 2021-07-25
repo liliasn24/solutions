b2068170f4cc990f95394077cc49a6db00d66e02
= <salaslilia00@gmail.com>
2021-07-25 17:16:11 -0400

    prime numbers completed

--- solutions.js
+++ solutions.js
@@ -33,3 +33,32 @@ const sumArray = (arr) => {
 }
 
 // console.log(sumArray([1, 2, 3, 4, 5, 6])); I couldnt get this to work when calling the function, but I'm gettim the sum af the numbers in the array.
+
+// Prime numbers
+//Step One
+let checkPrimes = (n) => {
+ if(n === 2){
+     return true;
+ }
+if(n < 2 || !(n % 2)){
+  return false;
+}
+for (let i = 3; i <= n-1; i++) {
+  if (n % i === 0) {
+    return false
+  }
+}
+return true
+}
+console.log(checkPrimes(19));
+
+// Step two
+let printPrime = (num) => {
+  for(let i = 0; i <= num; i++) {
+    if (checkPrimes(i)) {
+      console.log(i)
+    }
+  }
+}
+
+printPrime(100);
